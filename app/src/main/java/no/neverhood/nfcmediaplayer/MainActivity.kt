2568f51e98ccaff84a177379f28f58dce8c052a4
Gjermund Stensrud <gjermund@neverhood.no>
2026-03-20 00:52:48 +0100

    Added basic android views app with Youtube player and basic GUI

---
+++ app/src/main/java/no/neverhood/nfcmediaplayer/MainActivity.kt
@@ -0,0 +1,39 @@
+package no.neverhood.nfcmediaplayer
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+class MainActivity : AppCompatActivity() {
+    private var player: YouTubePlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Init YoutubePlayerView
+        val youTubePlayerView: YouTubePlayerView = findViewById(R.id.youtube_player_view)
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                player = youTubePlayer
+                // Process the intent that started the activity
+                //handleIntent(intent)
+            }
+        })
+    }
+}
